cda450fe095b230e1708151986a2b40a38cd4b02
Macbook <stephjaiye@gmail.com>
2024-06-24 22:42:45 +0530

    basic page

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-black">Login Page</h1>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
