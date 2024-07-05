f4b5844ef67abe709d769d3aeeb95cc7b9603864
Macbook <stephjaiye@gmail.com>
2024-07-05 16:27:37 +0530

    structure done

---
+++ pages/admin/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminLogin: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#0E214F]">
+      <h1 className="text-white text-3xl">Admin Login page</h1>
+    </div>
+  );
+};
+
+export default AdminLogin;
