b015fb1eff93f61966c288061528a6fcbd5adb62
Macbook <stephjaiye@gmail.com>
2024-08-21 11:55:35 +0530

    refactored

--- pages/register.tsx
+++ pages/register.tsx
@@ -145,11 +145,23 @@ const UserRegistration: React.FC = () => {
         </div>
 
         {/* Right side - Branding section */}
-        <div className="w-full lg:w-1/2 bg-[#F7F9FC] flex items-center justify-center">
+        <div className="w-full lg:w-1/2 bg-[#F7F9FC] flex items-center justify-center p-4">
           <div className="text-center">
-            <img src="/assets/logo.png" alt="Eco Media Player Logo" className="w-40 h-40 mx-auto mb-4" />
-            <h3 className="bg-[#0E214F] text-white text-sm px-2 py-1 mt-2 inline-block">Eco Media Player</h3>
-            <p className="text-gray-600 mt-12">watch movies and chill, you have got your cinema in your device</p>
+            {/* Container for Logo and Company Name */}
+            <div className="bg-gray-200 p-6 md:p-8 rounded-lg mb-4 flex flex-col items-center justify-center">
+              <img 
+                src="/assets/logo.png" 
+                alt="Eco Media Player Logo" 
+                className="w-32 h-32 md:w-48 md:h-40 mb-4"
+              />
+              <h3 className="bg-[#0E214F] text-white text-xs md:text-sm px-2 py-1 mt-2 inline-block">
+                Eco Media Player
+              </h3>
+            </div>
+            {/* Description Text */}
+            <p className="text-gray-600 mt-8 md:mt-12 font-poppins text-xs md:text-sm text-center">
+              watch movies and chill, <br /> you’ve got your cinema in your device
+            </p>
           </div>
         </div>
       </div>
